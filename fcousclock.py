daffb7a50c754739f77bcb47d53e37db4dbcd6ff
TomPops <132838143+TomPops@users.noreply.github.com>
2023-05-10 22:10:41 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,18 +1,13 @@
 import time
-import sys
 
-def countdown(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        t -= 1
+focus_time = 25 # 设置专注时间为25分钟
+total_time = focus_time * 60 # 计算总秒数
 
-    print('Time\'s up!')
-    sys.exit()
+while total_time:
+    mins, secs = divmod(total_time, 60)
+    time_str = f'{mins:02d}:{secs:02d}' # 格式化时间字符串
+    print(f'Current focus time: {time_str}', end='\r') # 输出当前时间，覆盖上一行输出
+    time.sleep(1) # 暂停1秒
+    total_time -= 1
 
-if __name__ == '__main__':
-    t = int(input("请输入要专注的时间（以分钟为单位）: "))
-    t *= 60
-    countdown(t)
+print('Focus time is over!') # 当专注时间结束时输出提示信息
